f9dcec4dbef9f2ddd9d361010d4ee493c4ae2dd3
Colby <colbykade626@gmail.com>
2022-02-11 01:21:57 -0600

    Added various folders and turn functuality and updated some packages

---
+++ Assets/Scripts/BattleMaster.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleMaster : MonoBehaviour
+{
+
+    bool battleStarted;
+    bool turnPassed;
+    public Text turn;
+    GameObject nextCharacter;
+    int characterindex = 0;
+    GameObject[] characters;
+
+    void Start()
+    {
+        //Finds all the participants and orders them from highest reflexes to lowest
+        characters = GameObject.FindGameObjectsWithTag("Participant");
+        for(int i = 0; i < characters.Length; i-=-1)
+        {
+            for(int j = 0; j < characters.Length - 1; j-=-1)
+            {
+                if (characters[j].GetComponent<CharacterSheet>().Reflexes < characters[j + 1].GetComponent<CharacterSheet>().Reflexes)
+                {
+                    GameObject temp = characters[j];
+                    characters[j] = characters[j + 1];
+                    characters[j + 1] = temp;
+                }
+            }  
+        }
+
+        turnPassed = false;
+        battleStarted = true;
+        nextCharacter = characters[0];
+    }
+
+    void Update()
+    {
+        //Displays the first character to go's name on the screen
+        if(battleStarted == true)
+        {
+            turn.text = "It is " + nextCharacter.gameObject.name + "'s turn";
+        }
+
+        //As each turn passes, displays the next character's name
+        if(turnPassed == true)
+        {
+            characterindex -= -1;
+            if (characterindex >= characters.Length)
+            {
+                characterindex = 0;
+            }
+            nextCharacter = characters[characterindex];
+            turnPassed = false;
+        }
+    }
+
+    //Used for the button to go to the next turn
+    public void NextTurn()
+    {
+        turnPassed = true;
+    }
+}
---
+++ Assets/Scripts/CharacterSheet.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSheet : MonoBehaviour
+{
+    public int Health;
+    public int Stamina;
+    [Header("Proficiencies:")]
+    public int FistProficiency;
+    public int LightProficiency;
+    public int One_handedProficiency;
+    public int Two_handedProficiency;
+    public int ReachProficiency;
+    public int BowProficiency;
+    public int CrossbowProficiency;
+    public int ThrownProficiency;
+    [Header("Stats:")]
+    public int Strength;
+    public int Attunement;
+    public int Reflexes;
+    public int Speed;
+    public int Precision;
+    public int Constitution;
+    public int Grit;
+    [Header("Equipment:")]
+    public GameObject HandSlot1;
+    public GameObject HandSlot2;
+    public GameObject Head;
+    public GameObject Torso;
+    public GameObject Legs;
+    public GameObject Feet;
+    public GameObject Arms;
+    public GameObject Ring1;
+    public GameObject Ring2;
+    public GameObject Neck;
+
+    //Bracelets and Rings go in the same slot
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
+    /*
+Fist
+
+Light: 
+    daggers, clubs, sickle, whip(has reach)
+
+One-handed:
+    longswords, axes, maces, flails(ignore shields)
+
+Two-handed:
+    greatswords, greataxes, greatclubs, greathammers, quarterstaffs
+
+Reach:
+    spears, swordstaffs, poleaxes, scythes(ignore shields)
+
+Bow:
+    shortbows, longbows
+
+Crossbow:
+    light crossbows, crossbows, heavy crossbows
+
+Thrown:
+    javelins, throwing daggers, throwing axes, slings
+     */
