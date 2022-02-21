fa468f61d39855a7c41c6a315b8fce53103015c0
Colby <colbykade626@gmail.com>
2022-02-21 16:18:54 -0600

    Added the start to attacking functionality

--- Assets/Scripts/CharacterSheet.cs
+++ Assets/Scripts/CharacterSheet.cs
@@ -34,17 +34,53 @@ public class CharacterSheet : MonoBehaviour
     public GameObject Ring1;
     public GameObject Ring2;
     public GameObject Neck;
+    [Header("Programmer stuff:")]
+    public float movementSpeed;
+
+    private Color startcolor;
 
     //Bracelets and Rings go in the same slot
 
-    void Start()
+    //Highlights the character when the mouse is over them 
+    void OnMouseEnter()
     {
-
+        startcolor = GetComponent<SpriteRenderer>().color;
+        GetComponent<SpriteRenderer>().color = GetComponent<SpriteRenderer>().color * 1.5f;
     }
 
-    void Update()
+    //When the mouse leaves the character they will be unhighlighted
+    void OnMouseExit()
     {
+        GetComponent<SpriteRenderer>().color = startcolor;
+    }
 
+    //Used for the attack button to deal damage
+    public void Attack(GameObject target)
+    {
+        //Make the movement happen over time
+        //Have a set target be highlighted by a mousepress
+        //Make the attack button start this function
+        int damage = Strength + 1;
+        Vector2 startPosition = gameObject.transform.position;
+        Vector2 targetPosition = target.gameObject.transform.position;
+        transform.position = Vector2.MoveTowards(startPosition, targetPosition, movementSpeed);
+        target.GetComponent<CharacterSheet>().TakeDamage(damage);
+        transform.position = Vector2.MoveTowards(targetPosition, startPosition, movementSpeed);
+    }
+
+    //Deals damage to this character
+    void TakeDamage(int damage)
+    {
+        Health -= damage;
+        if(Health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(this.gameObject);
     }
 }
     /*
