e2cac68382c4358ad878ae3fc186e3d342bc640a
Colby <colbykade626@gmail.com>
2022-11-07 16:38:01 -0600

    Removed all CodeMonkeys stuff

---
+++ Assets/Scripts/AI/Pathfinding.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinding
+{
+    private Grid<PathNode> grid;
+    public Pathfinding(int width, int height)
+    {
+        grid = new Grid<PathNode>(width, height, 1f, Vector3.zero, (Grid<PathNode> global, int x, int y) => new PathNode(g, x, y));
+    }
+}
