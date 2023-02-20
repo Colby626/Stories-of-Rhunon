eb6ad54fa90308a258a5c04fca788ca45c48d412
Colby <colbykade626@gmail.com>
2023-02-20 15:25:15 -0600

    connected demo scene to main menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -21,6 +21,12 @@ public class MainMenu : MonoBehaviour
         AudioManager.instance.StopAll();
         AudioManager.instance.Play("ExploringMusic");
     }
+    public void PlayShortButton()
+    {
+        SceneManager.LoadScene("GrassPlainsDemo");
+        AudioManager.instance.StopAll();
+        AudioManager.instance.Play("ExploringMusic");
+    }
 
     public void QuitButton()
     {
